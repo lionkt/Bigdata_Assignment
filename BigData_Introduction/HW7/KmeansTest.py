6f49deafaa63a33c2bbbeb74c7341be1cd508e71
lionkt <alien6608@outlook.com>
2018-04-22 02:54:16 +0800

    Finish basic version of Kmeans using lib-function.

--- BigData_Introduction/HW7/KmeansTest.py
+++ BigData_Introduction/HW7/KmeansTest.py
@@ -1,41 +1,51 @@
 """KmeansTest.py"""
 from numpy import array
 from math import sqrt
+import os
 
 from pyspark import SparkContext, SparkConf
 from pyspark.mllib.clustering import KMeans, KMeansModel
 
 
 # main loop
-# logFile = "/user/a2017210938/kddcup_convert.data_10_percent"  # Should be some file on your system
-logFile = "/user/a2017210938/README.md"  # Should be some file on your system
-
 conf = SparkConf().setAppName("KmeansTest")
-sc = SparkContext(logFile)
+sc = SparkContext(conf = conf)
 
-# # Load and parse the data
-# data = sc.textFile()
-# parsedData = data.map(lambda line: array([float(x) for x in line.split(',')]))
-#
-# # Build the model (cluster the data)
-# K = 23
-# clusters = KMeans.train(parsedData, K, maxIterations=10, initializationMode="random")
+# Load and parse the data
+logFile = "/user/a2017210938/kddcup_convert.data"  # Should be some file on your system
+data = sc.textFile(logFile)
+parsedData = data.map(lambda line: array([float(x) for x in line.split(',')]))
+
+# Build the model (cluster the data)
+K = 23
+clusters = KMeans.train(parsedData, K, maxIterations=1000, initializationMode="random")
+
 # Evaluate clustering by computing Within Set Sum of Squared Errors
-# def error(point):
-#     center = clusters.centers[clusters.predict(point)]
-#     return sqrt(sum([x ** 2 for x in (point - center)]))
-#
-# WSSSE = parsedData.map(lambda point: error(point)).reduce(lambda x, y: x + y)
-# print("Within Set Sum of Squared Error = " + str(WSSSE))
-#
-# # Save and load model
-# # clusters.save(sc, "./KMeansModel")
-# # sameModel = KMeansModel.load(sc, "./KMeansModel")
-#
-# sc.stop()
+def error(point):
+    center = clusters.centers[clusters.predict(point)]
+    # return sqrt(sum([x ** 2 for x in (point - center)]))
+    return sum([x ** 2 for x in (point - center)])
 
+WSSSE = parsedData.map(lambda point: error(point)).reduce(lambda x, y: x + y)
+print("Within Set Sum of Squared Error = " + str(WSSSE) + "\n")
 
-logData = sc.textFile(logFile).cache()
-numAs = logData.filter(lambda s: 'a' in s).count()
-numBs = logData.filter(lambda s: 'b' in s).count()
-print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
+# Save and load model
+clusters.save(sc, "./KMeansModel")
+sameModel = KMeansModel.load(sc, "./KMeansModel")
+
+# output kmeans center
+print("center number:%d" % len(sameModel.clusterCenters))       # KMeansModel.clusterCenters get 2-dim list
+print("sum of cost:%.2f" % sameModel.computeCost(parsedData))
+# print(os.path.split(os.path.realpath(__file__))[0])
+# fnew = open('/user/a2017210938/cluster_center.txt', 'w')
+# for th_ in range(len(sameModel.clusterCenters)):
+#     out_str = ""
+#     temp = sameModel.clusterCenters[th_]
+#     for i in range(len(temp) - 1):
+#         out_str = out_str + str(temp[i]) + ','
+#
+#     out_str = out_str + str(temp[-1]) + '\n'
+#     fnew.write(out_str)
+
+# fnew.close()
+sc.stop()
